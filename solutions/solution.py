662be686c0e83c120beaee55661bbb07d43af8d4
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-04-08 16:00:50 +0530

    Update solution.py

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,4 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums):
-        pass
-        #  implement your code
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
